d5ce5e9097e4ac474f86132180704179ea558674
IIC webteam <webteam2004@gmail.com>
2026-08-27 09:52:28 +0530

    Update admin login API endpoint to production URL

--- client/src/pages/adminLogin/AdminLogin.js
+++ client/src/pages/adminLogin/AdminLogin.js
@@ -15,7 +15,7 @@ function AdminLogin() {
         setError('');
 
         try {
-            const response = await fetch("http://localhost:5000/api/admin/login", {
+            const response = await fetch("https://react-iic.onrender.com/api/admin/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
